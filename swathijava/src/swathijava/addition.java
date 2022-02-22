ea14f7fa4d7e4a90e370f5d95e5fcc9a0ca41ca1
swathikuppala <kswathi515@gmail.com>
2022-02-22 20:41:43 +0530

    addition prg was added

---
+++ swathijava/src/swathijava/addition.java
@@ -0,0 +1,17 @@
+package swathijava;
+import java.util.*;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Scanner sc = new Scanner(System.in);
+      System.out.print("enter first number");
+      int a =sc.nextInt();
+      System.out.print("enter second num");
+      int b= sc.nextInt();
+      int c =a+b;
+      System.out.print("total=" +c);
+	}
+
+}
